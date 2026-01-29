1c571b66ee0f41880d6cfe4c0d5d4e273af0a0aa
Saf369 <safmu2090@gmail.com>
2026-01-30 03:28:02 +0530

    eighth commit

--- part2/countries/src/App.jsx
+++ part2/countries/src/App.jsx
@@ -1,34 +1,47 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState, useEffect } from "react"
+import Filter from "./components/Filter"
+import CountryList from "./components/CountryList"
+import CountryDetails from "./components/CountryDetails"
+import countriesService from "./services/countries"
 
-function App() {
-  const [count, setCount] = useState(0)
+
+const App = () => {
+  const [countries, setCountries] = useState([])
+  const [query, setQuery] = useState("")
+  const [showCountry, setShowCountry] = useState(null)
+
+  useEffect(() => {
+    countriesService.getAll().then(data => setCountries(data))
+  }, [])
+
+  const handleFilterChange = (e) => {
+    setQuery(e.target.value)
+    setShowCountry(null) // reset shown country
+  }
+
+  const filtered = countries.filter(c =>
+    c.name.common.toLowerCase().includes(query.toLowerCase())
+  )
+
+  const handleShow = (name) => {
+    const country = countries.find(c => c.name.common === name)
+    setShowCountry(country)
+  }
 
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <div>
+      <Filter value={query} onChange={handleFilterChange} />
+
+      {showCountry ? (
+        <CountryDetails country={showCountry} />
+      ) : filtered.length > 10 ? (
+        <p>Too many matches, specify another filter</p>
+      ) : filtered.length === 1 ? (
+        <CountryDetails country={filtered[0]} />
+      ) : (
+        <CountryList countries={filtered} onShow={handleShow} />
+      )}
+    </div>
   )
 }
 
